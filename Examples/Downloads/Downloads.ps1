6170a8a615fb7c88c90280204f70677955757b60
holbs <91827424+holbs@users.noreply.github.com>
2024-10-27 09:42:56 +0000

    Updates to description of downloads examples

--- Examples/Downloads/Downloads.ps1
+++ Examples/Downloads/Downloads.ps1
@@ -1,8 +1,8 @@
 <#
 .DESCRIPTION
     Downloads is used with ToastShell to show a toast notification prompt to the end user that they have items in their downloads older than 30 days
-    * If the user hits Review then the downloads folder is opened for the user to review the contents
-    * If the user hits Remove then any item older than 30 days in the downloads folder is deleted
+    * If the user hits Review then the downloads folder is opened for the user to review the contents, by running Show-Downloads, which is part of DownloadsCustomFunctions.ps1
+    * If the user hits Remove then any item older than 30 days in the downloads folder is deleted, by running Remove-Downloads, which is part of DownloadsCustomFunctions.ps1
 .NOTES
     Author : Aran Holbrook
     BlueSky: https://bsky.app/profile/holbs.bsky.social
